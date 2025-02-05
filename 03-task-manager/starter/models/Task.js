06fefc8db5137e948c4fba40888cf3ad966e3481
Victor-Herison <victorherisondev@gmail.com>
2025-02-05 16:59:22 -0300

    schema restricts

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-    task: String,
-    completed: Boolean
+    task: {
+        type: String,
+        required: [true, "a task precisa ser entitulada"],
+        trim: true,
+        maxlength: [200, 'a task não pode ter mais do que 200 letras']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 const Task = mongoose.model("tasks", taskSchema);
